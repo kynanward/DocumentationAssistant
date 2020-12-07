de20521c0523b3014fe252fc4e059f912f4d3bf4
jinyafeng <435643383@qq.com>
2020-12-07 21:20:05 +0800

    - p3

--- DocumentationAssistant/DocumentationAssistant.Config/ConfigurationPackage.cs
+++ DocumentationAssistant/DocumentationAssistant.Config/ConfigurationPackage.cs
@@ -32,13 +32,13 @@ namespace DocumentationAssistant.Config
 	/// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
 	/// </para>
 	/// </remarks>
-	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[PackageRegistration(UseManagedResourcesOnly = true)]
 	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
 	[Guid(ConfigurationPackage.PackageGuidString)]
 	[ProvideService(typeof(SOptionsService))]
 	[ProvideOptionPage(typeof(OptionsPage), "Documentation Assistant", "General from config", 0, 0, true)]
 	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-	public sealed class ConfigurationPackage : AsyncPackage
+	public sealed class ConfigurationPackage : Package
 	{
 		/// <summary>
 		/// ConfigurationPackage GUID string.
@@ -58,19 +58,8 @@ namespace DocumentationAssistant.Config
 
 		#region Package Members
 
-		/// <summary>
-		/// Initialization of the package; this method is called right after the package is sited, so this is the place
-		/// where you can put all the initialization code that rely on services provided by VisualStudio.
-		/// </summary>
-		/// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-		/// <param name="progress">A provider for progress updates.</param>
-		/// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
-		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+		protected override void Initialize()
 		{
-			// When initialized asynchronously, the current thread may be a background thread at this point.
-			// Do any initialization that requires the UI thread after switching to the UI thread.
-			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-
 			((IServiceContainer)this).AddService(typeof(SOptionsService), CreateService, true);
 		}
 
